7a17baa6e885a8889ee899bba36512737ef804b4
burnel9u <mathias.burnel9@etu.univ-lorraine.fr>
2026-02-05 09:46:10 +0100

    AJout modifs inscription/Connexions + homeCOntroller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,23 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; // Ajout requis
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController
+class HomeController extends AbstractController 
 {
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return new Response('<h1>Bienvenue sur ton site Symfony !</h1>');
+        $user = $this->getUser();
+
+        // Si l'utilisateur est connecté mais n'a pas validé le GDPR (validation modo)
+        if ($user && !$user->isAccordGdpr()) {
+            // On le redirige ou on affiche le message d'attente
+            return $this->render('security/waiting_validation.html.twig');
+        }
+
+        return $this->render('home/index.html.twig');
     }
 }
--- src/Entity/Utilisateur.php
+++ src/Entity/Utilisateur.php
@@ -4,9 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UtilisateurRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UtilisateurRepository::class)]
-class Utilisateur
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class Utilisateur implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -27,6 +31,9 @@ class Utilisateur
 
     #[ORM\Column]
     private ?bool $accordGdpr = null;
+
+    #[ORM\Column]
+    private ?bool $isModo = null;
 
     public function getId(): ?int
     {
@@ -92,4 +99,21 @@ class Utilisateur
 
         return $this;
     }
+
+    public function isModo(): ?bool
+    {
+        return $this->isModo;
+    }
+
+    public function setIsModo(bool $isModo): static
+    {
+        $this->isModo = $isModo;
+
+        return $this;
+    }
+
+    public function getUserIdentifier(): string { return (string) $this->email; }
+    public function getRoles(): array { return $this->isModo ? ['ROLE_MODO'] : ['ROLE_USER']; }
+    public function eraseCredentials(): void {}
+    public function getPassword(): ?string { return $this->mdp; }
 }
