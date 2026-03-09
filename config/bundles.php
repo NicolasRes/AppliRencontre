7d387b38b8e14696ea9ad919441d6fef8bc6010c
MinaroliCorentin <minaroli.corenin@gmail.com>
2026-03-09 23:25:19 +0100

    Activation des fixtures pour tous les environnements

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
