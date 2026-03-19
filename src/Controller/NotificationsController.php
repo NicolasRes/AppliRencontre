2c9b812a507ddc98b9a12b6134a5c922faee013a
Ryan <ryan.vergnaud4@etu.univ-lorraine.fr>
2026-03-19 16:22:13 +0100

    On peut plus accéder au notifs en modifiant l'url

--- src/Controller/NotificationsController.php
+++ src/Controller/NotificationsController.php
@@ -13,11 +13,12 @@ final class NotificationsController extends AbstractController
     #[Route('/notifications', name: 'app_notifications')]
     public function index(EntityManagerInterface $em): Response
     {
+        if(!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
         $notifs = null;
         $user = $this->getUser();
-        if($user){
-            $notifs = $em->getRepository(Notification::class)->findBy(['utilisateur' => $user]);
-        }
+        $notifs = $em->getRepository(Notification::class)->findBy(['utilisateur' => $user]);
         return $this->render('notifications/index.html.twig', [
             'notifs' => $notifs,
         ]);
