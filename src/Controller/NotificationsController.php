a51382c434f4dc4a884d5c637aa77e42689627df
burnel9u <mathias.burnel9@etu.univ-lorraine.fr>
2026-03-26 01:46:29 +0100

    ajout css pour notifications et login waiting

--- src/Controller/NotificationsController.php
+++ src/Controller/NotificationsController.php
@@ -13,14 +13,50 @@ final class NotificationsController extends AbstractController
     #[Route('/notifications', name: 'app_notifications')]
     public function index(EntityManagerInterface $em): Response
     {
-        if(!$this->getUser()){
+        // Vérification de la connexion [cite: 1]
+        if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
-        $notifs = null;
+
         $user = $this->getUser();
-        $notifs = $em->getRepository(Notification::class)->findBy(['utilisateur' => $user]);
+        
+        // On récupère les notifications de l'utilisateur connecté [cite: 1]
+        // Ajout d'un tri par ID DESC pour avoir les plus récentes en haut
+        $notifs = $em->getRepository(Notification::class)->findBy(
+            ['utilisateur' => $user],
+            ['id' => 'DESC']
+        );
+
         return $this->render('notifications/index.html.twig', [
             'notifs' => $notifs,
         ]);
+    }
+
+    #[Route('/notification/read/{id}', name: 'app_notification_read')]
+    public function read(Notification $notif, EntityManagerInterface $em): Response
+    {
+        // Sécurité : on vérifie que la notification appartient bien à l'utilisateur
+        if ($notif->getUtilisateur() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('Vous ne pouvez pas modifier cette notification.');
+        }
+
+        $notif->setLu(true); // Marque comme lu [cite: 1]
+        $em->flush();
+
+        return $this->redirectToRoute('app_notifications');
+    }
+
+    #[Route('/notification/delete/{id}', name: 'app_notification_delete')]
+    public function delete(Notification $notif, EntityManagerInterface $em): Response
+    {
+        // Sécurité : on vérifie que la notification appartient bien à l'utilisateur
+        if ($notif->getUtilisateur() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('Vous ne pouvez pas supprimer cette notification.');
+        }
+
+        $em->remove($notif); // Supprime de la BDD [cite: 1]
+        $em->flush();
+
+        return $this->redirectToRoute('app_notifications');
     }
 }
