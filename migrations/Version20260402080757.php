8256299aa10fd45d475d522edf7f3398b1b9c21d
MinaroliCorentin <minaroli.corenin@gmail.com>
2026-04-02 10:10:59 +0200

    migration

---
+++ migrations/Version20260402080757.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260402080757 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE configuration CHANGE genres_visibles genres_visibles JSON DEFAULT NULL');
+        $this->addSql('ALTER TABLE utilisateur CHANGE image_identite image_identite VARCHAR(50) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE configuration CHANGE genres_visibles genres_visibles LONGTEXT DEFAULT NULL COLLATE `utf8mb4_bin`');
+        $this->addSql('ALTER TABLE utilisateur CHANGE image_identite image_identite VARCHAR(50) DEFAULT \'NULL\'');
+    }
+}
