2d4405237884386a95974637c5f2a22a0fd3ccda
resende3u <nicolas.resende5@etu.univ-lorraine.fr>
2026-04-04 17:02:49 +0200

    Fix index et ajout méthode liste conversations (sauf utilisateur courant)

--- src/Controller/ConversationController.php
+++ src/Controller/ConversationController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Factory\ConversationFactory;
 use App\Entity\Utilisateur;
+use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,11 +16,32 @@ use App\Service\TopicService;
 use App\Entity\Conversation;
 
 /**
- * @method User|null getUser()
+ * @method Utilisateur|null getUser()
  */
 final class ConversationController extends AbstractController {
-    public function __construct (private readonly Authorization $authorization, private readonly Discovery $discovery, private readonly ConversationRepository $conversationRepository, private readonly TopicService $topicService, private readonly ConversationFactory $factory) {
+    public function __construct (private readonly Authorization $authorization,
+                                 private readonly Discovery $discovery,
+                                 private readonly ConversationRepository $conversationRepository,
+                                 private readonly TopicService $topicService,
+                                 private readonly ConversationFactory $factory,
+                                 private readonly UtilisateurRepository $utilisateurRepository) {
 
+    }
+
+    #[Route('/conversation', name: 'conversation.index')]
+    public function list(Request $request): Response
+    {
+        /** @var Utilisateur $user */
+        $user = $this->getUser();
+
+        $conversations = $user->getConversations();
+        $users = $this->utilisateurRepository->findOtherUsers($user);
+
+        return $this->render('conversation/chat.html.twig', [
+            'users' => $users,
+            'conversations' => $conversations,
+            'conversation' => null,
+        ]);
     }
 
     #[Route('/conversation/users/{recipient}', name: 'conversation.show')]
@@ -27,16 +49,23 @@ final class ConversationController extends AbstractController {
     {
         /** @var Utilisateur $sender */
         $sender = $this->getUser();
+        $users = $this->utilisateurRepository->findOtherUsers($sender);
         $conversation = $this->conversationRepository->findByUsers($sender, $recipient);
+
         if (!$conversation) {
             $conversation = $this->factory->create($sender, $recipient);
         }
+        $conversations = $sender->getConversations();
         $topic = $this->topicService->getTopicUrl($conversation);
+
         $this->discovery->addLink($request);
         $this->authorization->setCookie($request, [$topic]);
-        return $this->render('conversation/index.html.twig', ['conversation'=>$conversation, 'topic'=>$topic]);
+
+        return $this->render('conversation/chat.html.twig', [
+            'users' => $users,
+            'conversation'=>$conversation,
+            'topic'=>$topic,
+            'conversations' => $conversations]);
     }
 
 }
-
-?>
